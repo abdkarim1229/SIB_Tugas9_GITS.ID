cab6a064ed6c741263193b8c124db0f5f9d339d7
abdkarim1229 <abdulkarimklj@gmail.com>
2021-09-13 18:16:35 +0700

    first-commit

---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "SIB_TugaskeSembilan"
+
---
+++ src/main/kotlin/NestedandInner.kt
@@ -0,0 +1,32 @@
+class Bungkus {
+    //variabel untuk class inner
+    val asus = "Merk Laptop ASUS"
+
+    //membuat class nested
+    class Laptop {
+        //membuat variabel di dalam class nested
+        val n = "Merk Laptop Lenovo"
+
+        //membuat function didalam class nested
+        fun Merk() = "Merk Laptop ACER"
+    }
+
+    //membuat class inner
+    inner class Inner {
+        //membuat function dan memnnggil variabel b
+        fun MerkASUS() = asus
+    }
+}
+
+fun main() {
+    println("~ Nested Class ~")
+    //memanngil variabel di dalam class nested
+    println(Bungkus.Laptop().n)
+    //memanngil function didalam class nested
+    val laptop = Bungkus.Laptop()
+    println(laptop.Merk())
+    println("~ Inner Class ~")
+    //memanggil function di dalam class Inner menggunakan object asus
+    val asus = Bungkus()
+    println("Object asus : ${asus.Inner().MerkASUS()}")
+}
